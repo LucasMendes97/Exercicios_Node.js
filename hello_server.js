de7035d888b6bf6c678384b84b6563175d9cfcb1
LucasMendes97 <lucas.mendes.a@outlook.com>
2022-06-16 19:51:54 -0300

    main

--- hello_server.js
+++ hello_server.js
@@ -4,4 +4,4 @@ var server = http.createServer((request, response) => {
     response.write("<h1>Hello World!</h1>");
     response.end();
 });
-server.listen(3000);
+server.listen(3000, ()=>{console.log('Servidor Hello world rodando')});
