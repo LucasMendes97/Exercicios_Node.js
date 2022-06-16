73d73f0fe1fa0f12ff0f9e5cbea637be245d9248
LucasMendes97 <lucas.mendes.a@outlook.com>
2022-06-16 19:29:02 -0300

    main

--- hello_server.js
+++ hello_server.js
@@ -1,9 +1,7 @@
 var http = require('http');
-
 var server = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type:": "text/html" });
-    response.write("<h1>Hello world!</h1>");
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Hello World!</h1>");
     response.end();
 });
-
 server.listen(3000);
