c097c80f88fbb7f65f8b34f151aaed5c444cd196
LucasMendes97 <lucas.mendes.a@outlook.com>
2022-07-23 21:41:50 -0300

    main

--- site_pessoal.js
+++ site_pessoal.js
@@ -1,13 +1,37 @@
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer(function(request, response){
-// A constante __dirname retorna o diretório raiz da aplicação.
-fs.readFile(__dirname + '/index.html', function(err, html){
-response.writeHeader(200, {'Content-Type': 'text/html'});
-response.write(html);
-response.end();
+
+
+//Exemplo dele
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+
+// Funçao simples de gerenciamento a rotas.
+const rotear = function(pathname){
+  if (pathname && pathname !== '/') {
+    const arquivo = path.join(__dirname, `${pathname}.html`);
+    const existe = fs.existsSync(arquivo);
+    if (existe) {
+      return arquivo;
+    }
+    return path.join(__dirname, 'erro.html');
+  }
+  return path.join(__dirname, 'artigos.html');
+};
+
+// Iniciando Servidor do desafio
+const server = http.createServer((request, response) => {
+  // Obtendo o pathname digitado no browser
+  const pathname = url.parse(request.url).pathname;
+  // Processando roteamento do pathname
+  const pagina = rotear(pathname);
+  // Renderizando a pagina html
+  fs.readFile(pagina, (err, html) => {
+    response.writeHeader(200, {'Content-Type': 'text/html'});
+    response.end(html);
+  });
 });
-});
-server.listen(3000, function(){
-console.log(__dirname);
+
+server.listen(3000, () => {
+  console.log('Executando Desafio');
 });
